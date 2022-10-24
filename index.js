c1339f1101352de1181912a7f00e3baf21aebd8f
Khanya <khanya.keswa@gmail.com>
2022-10-24 15:18:51 -0500

    Phase 1 MVP mk.1

--- index.js
+++ index.js
@@ -9,56 +9,106 @@
 // console.log("This worked.")
 
 const citiesContainer = document.querySelector('#cities-container')
+const randomizerButton = document.querySelector('#randomize')
 
-// const cityData = {
-//   cityFullName: '',
-//   totalScore: '',
-//   imgUrl: '',
-// }
-
-function fetchCity(cityName) {
-  const cityData = {
-    cityFullName: '',
-    totalScore: '',
-    imgUrl: '',
-  }
-
-  fetch(`https://api.teleport.org/api/urban_areas/slug:${cityName}/`)
-    .then((r) => r.json())
-    .then((city) => {
-      cityData.cityFullName = city.full_name
-    })
-  fetch(`https://api.teleport.org/api/urban_areas/slug:${cityName}/scores/`)
-    .then((r) => r.json())
-    .then((scoresData) => {
-      cityData.totalScore = scoresData.teleport_city_score
-    })
-  fetch(`https://api.teleport.org/api/urban_areas/slug:${cityName}/images/`)
-    .then((r) => r.json())
-    .then((cityImage) => {
-      cityData.imgUrl = cityImage.photos[0].image.web
-      renderCity(cityData)
-    })
-
-  //  console.log(cityData)
-}
-
-fetchCity('san-francisco-bay-area')
-
-function renderCity(data) {
-  // console.log(data)
+function getCity() {
+  citiesContainer.innerHTML = ''
+  const randomIndex = Math.floor(Math.random() * 265)
   const cityCard = document.createElement('div')
 
   const cityName = document.createElement('h3')
-  cityName.textContent = data.cityFullName
-
   const cityScore = document.createElement('span')
-  cityScore.textContent = data.totalScore
 
+  const cityImageContainer = document.createElement('div')
   const cityImage = document.createElement('img')
-  cityImage.src = data.imgUrl
+  const cityDescription = document.createElement('span')
+  cityImageContainer.id = 'city-image-container'
+  cityImage.id = 'city-image'
+  cityImageContainer.append(cityDescription, cityImage)
 
-  cityCard.append(cityName, cityScore, cityImage)
+  const scoreList = document.createElement('ul')
+  
+  cityCard.append(cityName, cityScore, cityImageContainer, scoreList)
+
+
   citiesContainer.append(cityCard)
+
+  fetch('https://api.teleport.org/api/urban_areas/')
+    .then((r) => r.json())
+    .then((data) => {
+      const cityArray = data._links['ua:item']
+      const cityEntry = cityArray[randomIndex]
+
+      cityName.textContent = cityEntry.name
+
+      const cityURL = cityEntry.href
+
+      fetch(`${cityURL}scores`)
+        .then((r) => r.json())
+        .then((scoresData) => {
+          cityScore.textContent = Math.floor(scoresData.teleport_city_score)
+          scoresData.categories.forEach(category => {
+            const li = document.createElement('li')
+            li.textContent = `${category.name}: ${Math.floor(category.score_out_of_10)}`
+            scoreList.appendChild(li)
+
+            cityDescription.innerHTML = scoresData.summary
+            cityDescription.style.visibility = 'hidden'
+          })
+        })
+
+      fetch(`${cityURL}images`)
+        .then((r) => r.json())
+        .then((imgData) => {
+          cityImage.src = imgData.photos[0].image.web
+          cityDescription.style.position = 'absolute'
+        })
+    })
+
+    cityImageContainer.addEventListener('mouseover', () => {
+      cityImage.style.opacity = '0.15'
+      cityDescription.style.visibility = 'visible'
+    })
+
+    cityImageContainer.addEventListener('mouseleave', () => {
+      cityImage.style.opacity = '1'
+      cityDescription.style.visibility = 'hidden'
+    })
 }
 
+
+
+getCity()
+
+randomizerButton.addEventListener('click', getCity)
+
+// function fetchCity(cityName) {
+//   const cityCard = document.createElement('div')
+
+//   fetch(`https://api.teleport.org/api/urban_areas/slug:${cityName}/`)
+//     .then((r) => r.json())
+//     .then((city) => {
+//       const cityName = document.createElement('h3')
+//       cityName.textContent = city.full_name
+//       cityCard.append(cityName)
+//     })
+//   fetch(`https://api.teleport.org/api/urban_areas/slug:${cityName}/scores/`)
+//     .then((r) => r.json())
+//     .then((scoresData) => {
+//       const cityScore = document.createElement('span')
+//       cityScore.textContent = scoresData.teleport_city_score
+//       cityCard.append(cityScore)
+//     })
+//   fetch(`https://api.teleport.org/api/urban_areas/slug:${cityName}/images/`)
+//     .then((r) => r.json())
+//     .then((imgData) => {
+//       const cityImage = document.createElement('img')
+//       cityImage.src = imgData.photos[0].image.web
+//       cityCard.append(cityImage)
+//     })
+
+//   citiesContainer.append(cityCard)
+//   //  console.log(cityData)
+// }
+
+// fetchCity('san-francisco-bay-area')
