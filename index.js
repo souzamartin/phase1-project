9de84cb371d3804ab7ca50b43966ee889b2b9cd0
Khanya Keswa <khanya.keswa@gmail.com>
2022-10-25 13:25:54 -0500

    khanya branch changes merged, city tray completed

--- index.js
+++ index.js
@@ -3,8 +3,13 @@
 
 const searchForm = document.querySelector('#city-search-form')
 
+const topContainer = document.querySelector('#top-container')
 const citiesContainer = document.querySelector('#cities-container')
+const cityTray = document.querySelector('#city-tray')
 const randomizerButton = document.querySelector('#randomize')
+
+let tokenAdded = false
+let cityLink = ''
 
 searchForm.addEventListener('submit', (e) => {
   e.preventDefault()
@@ -25,41 +30,51 @@ searchForm.addEventListener('submit', (e) => {
 
   const scoreList = document.createElement('ul')
 
-  cityCard.append(cityName, cityScore, cumulativeMeter, cityImageContainer, scoreList)
-
+  cityCard.append(
+    cityName,
+    cityScore,
+    cumulativeMeter,
+    cityImageContainer,
+    scoreList
+  )
 
   citiesContainer.append(cityCard)
 
-  let searchString = e.target['city-search'].value.split(" ").join("-").toLowerCase()
+  let searchString = e.target['city-search'].value
+    .split(' ')
+    .join('-')
+    .toLowerCase()
   fetch(`https://api.teleport.org/api/urban_areas/slug:${searchString}/`)
-    .then(r => r.json())
-    .then(data => {
+    .then((r) => r.json())
+    .then((data) => {
       cityName.textContent = data.name
     })
-  
+
   fetch(`https://api.teleport.org/api/urban_areas/slug:${searchString}/scores`)
     .then((r) => r.json())
     .then((scoresData) => {
       cityScore.textContent = Math.floor(scoresData.teleport_city_score)
-        cumulativeMeter.id = cumulativeMeter
-        cumulativeMeter.min = 0
-        cumulativeMeter.max = 100
-        cumulativeMeter.low = 40
-        cumulativeMeter.high = 60
-        cumulativeMeter.optimum =80
-        cumulativeMeter.value = Math.floor(scoresData.teleport_city_score)
-      scoresData.categories.forEach(category => {
+      cumulativeMeter.id = cumulativeMeter
+      cumulativeMeter.min = 0
+      cumulativeMeter.max = 100
+      cumulativeMeter.low = 40
+      cumulativeMeter.high = 60
+      cumulativeMeter.optimum = 80
+      cumulativeMeter.value = Math.floor(scoresData.teleport_city_score)
+      scoresData.categories.forEach((category) => {
         const li = document.createElement('li')
-        li.textContent = `${category.name}: ${Math.floor(category.score_out_of_10)}`
+        li.textContent = `${category.name}: ${Math.floor(
+          category.score_out_of_10
+        )}`
 
         const categoryMeters = document.createElement('meter')
-            categoryMeters.id = category.score_out_of_10
-            categoryMeters.min = 0
-            categoryMeters.max = 10
-            categoryMeters.low = 4
-            categoryMeters.high = 6
-            categoryMeters.optimum =8
-            categoryMeters.value = `${Math.floor(category.score_out_of_10)}`
+        categoryMeters.id = category.score_out_of_10
+        categoryMeters.min = 0
+        categoryMeters.max = 10
+        categoryMeters.low = 4
+        categoryMeters.high = 6
+        categoryMeters.optimum = 8
+        categoryMeters.value = `${Math.floor(category.score_out_of_10)}`
 
         scoreList.append(li, categoryMeters)
 
@@ -88,13 +103,18 @@ searchForm.addEventListener('submit', (e) => {
   searchForm.reset()
 })
 
-
 function getCity() {
   citiesContainer.innerHTML = ''
   const randomIndex = Math.floor(Math.random() * 265)
   const cityCard = document.createElement('div')
+  cityCard.id = 'city-card'
 
   const cityName = document.createElement('h3')
+  cityName.id = 'city-name'
+  const saveButton = document.createElement('button')
+  saveButton.id = 'save-button'
+  saveButton.innerText = 'Save City'
+  topContainer.appendChild(saveButton)
   const cityScore = document.createElement('span')
   const cumulativeMeter = document.createElement('meter')
 
@@ -106,9 +126,14 @@ function getCity() {
   cityImageContainer.append(cityDescription, cityImage)
 
   const scoreList = document.createElement('ul')
-  
-  cityCard.append(cityName, cityScore, cumulativeMeter, cityImageContainer, scoreList)
 
+  cityCard.append(
+    cityName,
+    cityScore,
+    cumulativeMeter,
+    cityImageContainer,
+    scoreList
+  )
 
   citiesContainer.append(cityCard)
 
@@ -121,30 +146,33 @@ function getCity() {
       cityName.textContent = cityEntry.name
 
       const cityURL = cityEntry.href
+      cityLink = cityEntry.href
 
       fetch(`${cityURL}scores`)
         .then((r) => r.json())
         .then((scoresData) => {
           cityScore.textContent = Math.floor(scoresData.teleport_city_score)
-            cumulativeMeter.id = cumulativeMeter
-            cumulativeMeter.min = 0
-            cumulativeMeter.max = 100
-            cumulativeMeter.low = 40
-            cumulativeMeter.high = 60
-            cumulativeMeter.optimum =80
-            cumulativeMeter.value = Math.floor(scoresData.teleport_city_score)
-          scoresData.categories.forEach(category => {
+          cumulativeMeter.id = cumulativeMeter
+          cumulativeMeter.min = 0
+          cumulativeMeter.max = 100
+          cumulativeMeter.low = 40
+          cumulativeMeter.high = 60
+          cumulativeMeter.optimum = 80
+          cumulativeMeter.value = Math.floor(scoresData.teleport_city_score)
+          scoresData.categories.forEach((category) => {
             const li = document.createElement('li')
-            li.textContent = `${category.name}: ${Math.floor(category.score_out_of_10)}`
+            li.textContent = `${category.name}: ${Math.floor(
+              category.score_out_of_10
+            )}`
 
             const categoryMeters = document.createElement('meter')
-              categoryMeters.id = category.score_out_of_10
-              categoryMeters.min = 0
-              categoryMeters.max = 10
-              categoryMeters.low = 4
-              categoryMeters.high = 6
-              categoryMeters.optimum =8
-              categoryMeters.value = `${Math.floor(category.score_out_of_10)}`
+            categoryMeters.id = category.score_out_of_10
+            categoryMeters.min = 0
+            categoryMeters.max = 10
+            categoryMeters.low = 4
+            categoryMeters.high = 6
+            categoryMeters.optimum = 8
+            categoryMeters.value = `${Math.floor(category.score_out_of_10)}`
 
             scoreList.append(li, categoryMeters)
 
@@ -160,24 +188,137 @@ function getCity() {
           cityDescription.style.position = 'absolute'
         })
     })
+  saveButton.addEventListener('click', () => {
+    if (!tokenAdded) {
+      saveButton.remove()
+      cityTray.style.height = '150px'
+      const cityToken = document.createElement('div')
+      cityToken.className = 'city-token'
+      cityToken.id = cityLink
+      const tokenOverlay = document.createElement('div')
+      tokenOverlay.id = 'token-overlay'
+      tokenOverlay.textContent = cityName.textContent
+      cityToken.appendChild(tokenOverlay)
+      cityToken.style.backgroundImage = `url(${cityImage.src})`
+      cityTray.append(cityToken)
 
-    cityImageContainer.addEventListener('mouseover', () => {
-      cityImage.style.opacity = '0.15'
-      cityDescription.style.visibility = 'visible'
-    })
+      cityToken.addEventListener('click', () => {
+        // if (cityTray.childElementCount <= 1) {
+        //   cityTray.style.height = '0px'
+        // }
 
-    cityImageContainer.addEventListener('mouseleave', () => {
-      cityImage.style.opacity = '1'
-      cityDescription.style.visibility = 'hidden'
-    })
+        // cityToken.remove()
+        citiesContainer.innerHTML = ''
+        const cityCard = document.createElement('div')
+        cityCard.id = 'city-card'
+
+        const cityName = document.createElement('h3')
+        cityName.id = cityName
+        const saveButton = document.createElement('button')
+        saveButton.id = 'save-button'
+        saveButton.innerText = 'Save City'
+        if (!document.getElementById('save-button')) {
+          topContainer.appendChild(saveButton)
+        }
+
+        const cityScore = document.createElement('span')
+        const cumulativeMeter = document.createElement('meter')
+
+        const cityImageContainer = document.createElement('div')
+        const cityImage = document.createElement('img')
+        const cityDescription = document.createElement('span')
+        cityImageContainer.id = 'city-image-container'
+        cityImage.id = 'city-image'
+        cityImageContainer.append(cityDescription, cityImage)
+
+        const scoreList = document.createElement('ul')
+
+        cityCard.append(
+          cityName,
+          cityScore,
+          cumulativeMeter,
+          cityImageContainer,
+          scoreList
+        )
+
+        citiesContainer.append(cityCard)
+
+        fetch(`${cityToken.id}`)
+          .then((r) => r.json())
+          .then((tokenCity) => {
+            cityName.textContent = tokenCity.name
+
+            fetch(`${cityToken.id}scores`)
+              .then((r) => r.json())
+              .then((scoresData) => {
+                cityScore.textContent = Math.floor(
+                  scoresData.teleport_city_score
+                )
+                cumulativeMeter.id = cumulativeMeter
+                cumulativeMeter.min = 0
+                cumulativeMeter.max = 100
+                cumulativeMeter.low = 40
+                cumulativeMeter.high = 60
+                cumulativeMeter.optimum = 80
+                cumulativeMeter.value = Math.floor(
+                  scoresData.teleport_city_score
+                )
+                scoresData.categories.forEach((category) => {
+                  const li = document.createElement('li')
+                  li.textContent = `${category.name}: ${Math.floor(
+                    category.score_out_of_10
+                  )}`
+
+                  const categoryMeters = document.createElement('meter')
+                  categoryMeters.id = category.score_out_of_10
+                  categoryMeters.min = 0
+                  categoryMeters.max = 10
+                  categoryMeters.low = 4
+                  categoryMeters.high = 6
+                  categoryMeters.optimum = 8
+                  categoryMeters.value = `${Math.floor(
+                    category.score_out_of_10
+                  )}`
+
+                  scoreList.append(li, categoryMeters)
+
+                  cityDescription.innerHTML = scoresData.summary
+                  cityDescription.style.visibility = 'hidden'
+                })
+              })
+
+            fetch(`${cityToken.id}images`)
+              .then((r) => r.json())
+              .then((imgData) => {
+                cityImage.src = imgData.photos[0].image.web
+                cityDescription.style.position = 'absolute'
+              })
+          })
+      })
+    }
+    tokenAdded = true
+  })
+
+  cityImageContainer.addEventListener('mouseover', () => {
+    cityImage.style.opacity = '0.15'
+    cityDescription.style.visibility = 'visible'
+  })
+
+  cityImageContainer.addEventListener('mouseleave', () => {
+    cityImage.style.opacity = '1'
+    cityDescription.style.visibility = 'hidden'
+  })
 }
-
-
 
 getCity()
 
-randomizerButton.addEventListener('click', getCity)
-
+randomizerButton.addEventListener('click', () => {
+  if (document.getElementById('save-button')) {
+    document.getElementById('save-button').remove()
+  }
+  getCity()
+  tokenAdded = false
+})
 // function fetchCity(cityName) {
 //   const cityCard = document.createElement('div')
 
